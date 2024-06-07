d2cd5f197bb73d810a9d63b0300323ed953eaeb9
JoeGuerraR <Joeguerraroman@gmail.comn>
2024-06-07 18:46:46 -0500

    Inicio de Examen

---
+++ src/main/java/pe/edu/cibertec/DAAII_T1_Joe_Guerra/DaaiiT1JoeGuerraApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DAAII_T1_Joe_Guerra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaaiiT1JoeGuerraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaaiiT1JoeGuerraApplication.class, args);
+	}
+
+}
